f2fc2527bf6a992e56cf97faab7d38e6693ce2c2
Jonathan Apodaca <jonathan.apodaca@colostate.edu>
2017-04-14 16:31:37 -0600

    Update USFM parser; things look a bit better

--- src/main/apps/bible/renderer.tsx
+++ src/main/apps/bible/renderer.tsx
@@ -1,16 +1,6 @@
 import * as React from 'react'
 import BibleModel from './lib/bible'
 
-function space(els: any[]) {
-	const newEls = []
-	els.forEach((e, i) => {
-		newEls.push(e)
-		if (i + 1 < els.length)
-			newEls.push(<span key={els.length + i}>&nbsp;</span>)
-	})
-	return newEls
-}
-
 const RENDERERS: Map<string, any> = new Map()
 function register(type: string, c: any) {
 	RENDERERS.set(type, c)
@@ -24,20 +14,24 @@ function renderer(node: any) {
 const nothing = (type: string) => register(type, () => <span></span>)
 
 nothing('id')
+nothing('ide')
 nothing('h')
 nothing('toc1')
 nothing('toc2')
 nothing('toc3')
 
 register('bk', props => <span><Node node={props.node.value} /></span>)
+register('ili', props => <ul><li><Node node={props.node.ili} /></li></ul>)
 register('ip', props => <p><Node node={props.node.ip} /></p>)
 register('is1', props => <h2><Node node={props.node.is1} /></h2>)
 register('mt1', props => <h1><Node node={props.node.mt1} /></h1>)
+register('mt2', props => <h2><Node node={props.node.mt2} /></h2>)
+register('mt3', props => <h3><Node node={props.node.mt3} /></h3>)
 
 function Node(props: {node: any}) {
 	if (Array.isArray(props.node)) {
 		return <span>
-			{space(props.node.map((node, i) => <Node key={i} node={node} />))}
+			{props.node.map((node, i) => <Node key={i} node={node} />)}
 		</span>
 	} else if (typeof props.node == 'string') {
 		return <span>{props.node}</span>
@@ -61,7 +55,7 @@ export default class Renderer extends React.Component<any, any> {
 			h: string
 		} = this.props.source
 
-		const [book] = this.props.bible.books.filter(book => book.h == this.props.source.h)
+		const [book] = this.props.bible.books.filter(book => book.h.trim() == this.props.source.h.trim())
 		return <div>
 			{book.ast.map((node, i) => <Node key={i} node={node} />)}
 		</div>
